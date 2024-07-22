79413910c83b0afe741bcf48b6fc03c1715e16d8
Konstantin Savin <110243772+KonstantinSavin@users.noreply.github.com>
2024-07-22 16:18:36 +0300

    Добавил новую задачу

--- 374.GuessNumberHigherOrLower/main.go
+++ 374.GuessNumberHigherOrLower/main.go
@@ -1,15 +1,15 @@
 package main
 
-func guessNumber(n int) int {
-	l, r := 1, n
-	var m int
-	for l < r {
-		m = l + (r-l)/2
-		if guess(m) <= 0 {
-			r = m
-		} else {
-			l = m + 1
-		}
-	}
-	return l
-}
+// func guessNumber(n int) int {
+// 	l, r := 1, n
+// 	var m int
+// 	for l < r {
+// 		m = l + (r-l)/2
+// 		if guess(m) <= 0 {
+// 			r = m
+// 		} else {
+// 			l = m + 1
+// 		}
+// 	}
+// 	return l
+// }
